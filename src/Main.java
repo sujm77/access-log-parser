31f2216696613f0b752b62e5661d2a2487f978cd
Bakanova Oksana <sujm1977@gmail.com>
2024-12-15 19:32:49 +0300

    Действия над двумя числами

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>");
-       String text = new Scanner(System.in).nextLine();
-       System.out.println("Длина текста: "+text.length());
+        System.out.println("Введите первое число");
+        int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число");
+        int number2 = new Scanner(System.in).nextInt();
+        int sum = number1+number2;
+        System.out.println("Сумма чисел : "+sum);
+        int dif = number1-number2;
+        System.out.println("Разность чисел : "+ dif);
+        int comp = number1*number2;
+        System.out.println("Произведение чисел : "+comp);
+        double quotient = (double) number1/number2;
+        System.out.println("Частное чисел : "+quotient);
     }
 }
